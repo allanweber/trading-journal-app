b8eebe9b725a3998f4b23ecab50f6248d0595be0
Allan <a.cassianoweber@gmail.com>
2023-01-04 20:51:44 +0100

    Fix Entry Filter Mobile

--- src/components/entry-select/index.jsx
+++ src/components/entry-select/index.jsx
@@ -1,12 +1,28 @@
-import { Box } from '@mui/material';
+import ContactSupportOutlinedIcon from '@mui/icons-material/ContactSupportOutlined';
+import { Box, IconButton, Popover, Stack, Tooltip } from '@mui/material';
 import ToggleButton from '@mui/material/ToggleButton';
 import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
 import Typography from '@mui/material/Typography';
 import React, { useState } from 'react';
 import { types } from '../../containers/entries/EntryTypes';
+import { useIsMobile } from '../../hooks/useIsMobile';
 
 export const EntrySelect = ({ onChange }) => {
+  const isMobile = useIsMobile();
   const [type, setType] = useState('TRADE');
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+  const id = open ? 'simple-popover' : undefined;
+
   const changeFilter = (type) => {
     setType(type);
     if (onChange) {
@@ -20,17 +36,65 @@ export const EntrySelect = ({ onChange }) => {
         value={type}
         onChange={(e, value) => changeFilter(value)}
         exclusive
-        size="small"
+        size={isMobile ? 'medium' : 'small'}
       >
-        {types.map((type) => (
-          <ToggleButton value={type.key} key={type.key}>
-            {React.cloneElement(type.icon, {
-              fontSize: 'small',
-            })}
-            <Typography sx={{ ml: 1 }}>{type.value}</Typography>
-          </ToggleButton>
-        ))}
+        {types.map((type) => {
+          if (isMobile) {
+            return (
+              <ToggleButton value={type.key} key={type.key}>
+                <Tooltip title={type.value}>
+                  {React.cloneElement(type.icon, {
+                    fontSize: 'small',
+                  })}
+                </Tooltip>
+              </ToggleButton>
+            );
+          } else {
+            return (
+              <ToggleButton value={type.key} key={type.key}>
+                {React.cloneElement(type.icon, {
+                  fontSize: 'small',
+                })}
+                <Typography sx={{ ml: 1 }}>{type.value}</Typography>
+              </ToggleButton>
+            );
+          }
+        })}
       </ToggleButtonGroup>
+      {isMobile && (
+        <Box display="inline">
+          <IconButton
+            aria-label="help"
+            aria-describedby={id}
+            onClick={handleClick}
+          >
+            <ContactSupportOutlinedIcon />
+          </IconButton>
+          <Popover
+            id={id}
+            open={open}
+            anchorEl={anchorEl}
+            onClose={handleClose}
+            anchorOrigin={{
+              vertical: 'center',
+              horizontal: 'right',
+            }}
+            transformOrigin={{
+              vertical: 'center',
+              horizontal: 'left',
+            }}
+          >
+            {types.map((type) => (
+              <Stack direction="row" spacing={1} key={type.key}>
+                {React.cloneElement(type.icon, {
+                  fontSize: 'small',
+                })}
+                <Typography sx={{ ml: 1 }}>{type.value}</Typography>
+              </Stack>
+            ))}
+          </Popover>
+        </Box>
+      )}
     </Box>
   );
 };
