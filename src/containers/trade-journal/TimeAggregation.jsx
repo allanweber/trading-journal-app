dc50fe497c1536f7a15ae7ce0219aabf5657b101
Allan <a.cassianoweber@gmail.com>
2023-01-24 18:40:07 +0100

    Page time aggregation

--- src/containers/trade-journal/TimeAggregation.jsx
+++ src/containers/trade-journal/TimeAggregation.jsx
@@ -1,5 +1,12 @@
 import ChevronRightOutlinedIcon from '@mui/icons-material/ChevronRightOutlined';
-import { Box, Divider, Grid, Link, Typography, useTheme } from '@mui/material';
+import {
+  Box,
+  Divider,
+  Grid,
+  TablePagination,
+  Typography,
+  useTheme,
+} from '@mui/material';
 import React, { useEffect, useState } from 'react';
 import { Alert } from '../../components/alert';
 import { ColorfulCurrency } from '../../components/colorful-currency';
@@ -9,16 +16,15 @@ import { useColors } from '../../hooks/useColors';
 import { useAggregateTime } from '../../services/TradeQueries';
 import { ShortDisplay } from './ShortDisplay';
 
-export const TimeAggregation = ({ journal }) => {
+export const TimeAggregation = ({ journal, onChange }) => {
   const colors = useColors();
   const theme = useTheme();
   const [aggregation, setAggregation] = useState('DAY');
-  const [timesMap, setTimesMap] = useState([]);
-  const [selected, setSelected] = useState('');
+  const [selected, setSelected] = useState(undefined);
   const [page, setPage] = useState(0);
 
   const {
-    data: times,
+    data: periodGroups,
     isFetching,
     error,
   } = useAggregateTime(journal.id, aggregation, page, 10);
@@ -29,35 +35,41 @@ export const TimeAggregation = ({ journal }) => {
   };
 
   useEffect(() => {
-    if (times) {
-      times.forEach((time) => {
-        const existentGroup = timesMap.find(
-          (group) => group.group === time.group
-        );
-        if (existentGroup !== undefined) {
-          const index = timesMap.indexOf(existentGroup);
-          const items = timesMap[index].items.concat(time.items);
-          timesMap[index].items = items;
-          setTimesMap(timesMap);
-        } else {
-          timesMap.push(time);
-          setTimesMap(timesMap);
-        }
-      });
-    }
-  }, [times, timesMap]);
+    return () => {
+      setPage(0);
+    };
+  }, []);
 
   useEffect(() => {
-    setTimesMap([]);
+    if (periodGroups && periodGroups.items) {
+      const [firstPeriod] = periodGroups.items;
+      if (firstPeriod) {
+        const [firstItem] = firstPeriod.items;
+        if (firstItem) {
+          const select = { time: firstItem.group, aggregation };
+          setSelected(select);
+        }
+      }
+    }
+  }, [periodGroups, aggregation]);
+
+  useEffect(() => {
     setPage(0);
   }, [aggregation]);
 
   const selectTime = (item) => {
-    setSelected(item.group);
+    const select = { time: item.group, aggregation };
+    setSelected(select);
   };
 
-  const loadMore = () => {
-    setPage(page + 1);
+  useEffect(() => {
+    if (onChange) {
+      onChange(selected);
+    }
+  }, [selected, onChange]);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
   };
 
   return (
@@ -89,8 +101,8 @@ export const TimeAggregation = ({ journal }) => {
           overflowY: 'scroll',
         }}
       >
-        {timesMap &&
-          timesMap.map((time) => (
+        {periodGroups &&
+          periodGroups.items.map((time) => (
             <Box
               key={`group-${time.group}`}
               marginTop={2}
@@ -113,52 +125,71 @@ export const TimeAggregation = ({ journal }) => {
                 </Typography>
               </Divider>
 
-              {time.items &&
-                time.items.map((item) => (
-                  <Box
-                    onClick={() => selectTime(item)}
-                    key={`item-${item.group}`}
-                    display="flex"
-                    direction="row"
-                    justifyContent="start"
-                    className={item.group === selected ? 'selected' : null}
-                    p={2}
-                    marginTop={1}
-                    sx={{
-                      '&:hover': {
-                        cursor: 'pointer',
-                        backgroundColor: `${
-                          theme.palette.mode === 'dark'
-                            ? colors.primary[400]
-                            : colors.primary[400]
-                        }`,
-                      },
-                    }}
-                  >
+              {time.items.map((item) => (
+                <Box
+                  onClick={() => selectTime(item)}
+                  key={`item-${item.group}`}
+                  display="flex"
+                  direction="row"
+                  justifyContent="start"
+                  className={
+                    selected && item.group === selected.time ? 'selected' : null
+                  }
+                  p={2}
+                  marginTop={1}
+                  sx={{
+                    '&:hover': {
+                      cursor: 'pointer',
+                      backgroundColor: `${
+                        theme.palette.mode === 'dark'
+                          ? colors.primary[400]
+                          : colors.primary[400]
+                      }`,
+                    },
+                  }}
+                >
+                  <Box>
                     <Box>
-                      <Box>
-                        <Typography variant="h5" fontWeight="bold">
-                          <ShortDisplay date={item.group} type={aggregation} />
-                        </Typography>
-                      </Box>
-                      <ColorfulCurrency
-                        value={item.result}
-                        currency={journal.currency}
-                      />
+                      <Typography variant="h5" fontWeight="bold">
+                        <ShortDisplay date={item.group} type={aggregation} />
+                      </Typography>
                     </Box>
-                    <Box sx={{ ml: 'auto' }} marginTop="5px">
-                      <ChevronRightOutlinedIcon fontSize="large" />
-                    </Box>
+                    <ColorfulCurrency
+                      value={item.result}
+                      currency={journal.currency}
+                    />
                   </Box>
-                ))}
+                  <Box sx={{ ml: 'auto' }} marginTop="5px">
+                    <ChevronRightOutlinedIcon fontSize="large" />
+                  </Box>
+                </Box>
+              ))}
             </Box>
           ))}
 
-        {times && (
-          <Box display="flex" justifyContent="center" alignItems="center">
-            <Link href="#" underline="none" onClick={loadMore}>
-              Load more
-            </Link>
+        {periodGroups && (
+          <Box
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+            sx={{
+              '& .MuiTablePagination-toolbar': {
+                pr: 0,
+                pl: 0,
+              },
+            }}
+          >
+            <TablePagination
+              component="div"
+              count={periodGroups.total}
+              page={page}
+              rowsPerPage={10}
+              labelRowsPerPage=""
+              rowsPerPageOptions={[10]}
+              showFirstButton={true}
+              showLastButton={true}
+              onPageChange={handleChangePage}
+            />
           </Box>
         )}
       </Box>
--- src/containers/trade-journal/TradeJournal.jsx
+++ src/containers/trade-journal/TradeJournal.jsx
@@ -3,10 +3,14 @@ import React from 'react';
 import { TimeAggregation } from './TimeAggregation';
 
 export const TradeJournal = ({ journal }) => {
+  const timeAggregationChange = (time) => {
+    console.log(time);
+  };
+
   return (
     <Grid container direction="row" justifyContent="start">
       <Box padding={1} minWidth="250px">
-        <TimeAggregation journal={journal} />
+        <TimeAggregation journal={journal} onChange={timeAggregationChange} />
       </Box>
       <Box padding={1} paddingLeft={2} minWidth="250px">
         <label>a</label>
